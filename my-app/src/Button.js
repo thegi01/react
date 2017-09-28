a11d432dcb5260f2c367391d0091d2ab7bdfcbcf
thegi <deokim77@gmail.com>
2017-09-28 23:26:49 +0900

    no message

---
+++ my-app/src/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Button.css'
+
+class Button extends Component {
+  render() {
+    return(
+        <button type="button" className="button">Button Default</button>
+    );
+  }
+}
+
+export default Button;
