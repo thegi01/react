60b4f25cbc6d923b18a0cd1438886820db7faae8
thegi <deokim77@gmail.com>
2017-11-02 00:02:37 +0900

    dddd

--- myApp/src/App.js
+++ myApp/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DangerButton from './DangerButton';
 
 class App extends Component {
+    handleClick = () => {
+        import('./moduleA')
+            .then( ({moduleA}) => {
+                console.log({moduleA});
+            })
+            .catch( err => {
+                console.log(err);
+            });
+    }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +23,10 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+            <DangerButton/>
+            <button type="button" onClick={this.handleClick}> Load </button>
         </p>
       </div>
     );
---
+++ myApp/src/DangerButton.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import Button from './Button';
+
+class DangerButton extends Component {
+    render(){
+        return <Button color="red"> Danger Button </Button>
+    }
+}
+
+export default DangerButton;
