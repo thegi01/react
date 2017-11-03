4d847c54bdf76c801489affc21791f468e764b08
thegi <deokim77@gmail.com>
2017-11-03 21:56:58 +0900

    router 정리

--- react-router/src/components/App.js
+++ react-router/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Header from './Header'
 import Main from './Main'
+import './App.css'
 
 const App = () => (
-  <div>
+  <div className="App">
     <Header />
     <Main />
   </div>
